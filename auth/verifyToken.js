98ade9a83d0b8f4b5ebb9669ae69f7db51023b9d
Sanitha S <sanithasudevan@gmail.com>
2024-11-03 17:42:03 +0530

    Token Verification

---
+++ auth/verifyToken.js
@@ -0,0 +1,34 @@
+import jwt from "jsonwebtoken";
+import User from "../models/UserSchema.js";
+
+
+export const authenticate = ( req, res, next ) =>
+{
+    const authToken = req.headers.authorization;
+
+    console.log( "showing autherization:", req.headers.authorization );
+
+    if ( !authToken || authToken.startsWith( "Bearer" ) )
+    {
+        return res.status( 404 ).json( { succes: false, message: "Authorization Denied" } )
+
+    }
+    try
+    {
+        const token = authToken.split( "" )[ 1 ];
+
+        const decoded = jwt.verify( token, process.env.JWT_SECRET_KEY );
+        req.userId = decoded.id;
+        req.role = decoded.role;
+        next();
+
+    }
+    catch ( error )
+    {
+        if ( error.name === "TokenExpiredError" )
+        {
+            res.status( 401 ).json( { succes: false, message: "Token Expired" } )
+        }
+        res.status( 401 ).json( { succes: false, message: "Invalid Token.Please login again" } )
+    }
+};
